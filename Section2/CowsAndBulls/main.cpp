f64c68f40bb77ea6fe7991df9b2750ae2005c0e6
Vincent Finn <vincent.s.finn@gmail.com>
2019-04-06 18:35:37 -0400

    midway point

--- Section2/CowsAndBulls/main.cpp
+++ Section2/CowsAndBulls/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <string>
+#include "main.h"
 
 using namespace std;
 
-int main() {
-	//Take the input and put it back here
-	cout << "Test" << endl;
+void PrintIntro();
+string GetGuessAndPrint();
+
+//entry point for our application
+int main()
+{
+
+	PrintIntro();
+	for (int count = 1; count <= 5; count++) {
+		GetGuessAndPrint();
+		cout << endl;
+	}
+
 	return 0;
+}
+
+string GetGuessAndPrint()
+{
+	string Guess = "";
+
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+	cout << endl;
+
+	//print the guess back
+	cout << "Your guess was: " << Guess << endl;
+
+	return Guess;
+}
+
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 }
