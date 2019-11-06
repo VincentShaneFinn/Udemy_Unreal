05a1ba07d36e6135199e00ba2631bc1f4a4c18ed
Vincent Finn <vincent.s.finn@gmail.com>
2019-11-05 21:56:28 -0500

    removed and added new project

---
+++ UnrealUdemy/Source/UnrealUdemy/UnrealUdemyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealUdemyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALUDEMY_API AUnrealUdemyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
